97e5c03fe9e069da426c3a4194ddcdefdd38c998
Otso Jousimaa <otso@ojousima.net>
2018-02-28 19:00:29 +0800

    Bidir NFC

--- application_config/application_config.h
+++ application_config/application_config.h
@@ -13,6 +13,21 @@
 #define NFC_APP_BUF_SIZE 64
 #define NFC_DATA_BUF_SIZE 240
 #define NDEF_FILE_SIZE 512
+#define NFC_NDEF_MSG_PARSER_LOG_ENABLED 1
+#define NFC_NDEF_MSG_PARSER_LOG_LEVEL   3 //INFO
+#define NFC_NDEF_MSG_PARSER_INFO_COLOR  0 //Black
+#define NFC_NDEF_RECORD_PARSER_LOG_ENABLED 1
+#define NFC_NDEF_RECORD_PARSER_LOG_LEVEL 3
+#define NFC_NDEF_RECORD_PARSER_INFO_COLOR 0
+
+// <i> The following modes are supported:
+// <i> - SKIP  - Do not block, output nothing.
+// <i> - TRIM  - Do not block, output as much as fits.
+// <i> - BLOCK - Wait until there is space in the buffer.
+// <0=> SKIP 
+// <1=> TRIM 
+// <2=> BLOCK_IF_FIFO_FULL 
+#define SEGGER_RTT_CONFIG_DEFAULT_MODE 2
 
 #define APPLICATION_FLOAT_USE
 
