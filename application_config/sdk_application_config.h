4d83b0937eeabddcf9a1da6a84a2149ea8be8065
Otso Jousimaa <otso@ojousima.net>
2018-03-12 14:09:15 +0700

    Add support for NFC TX

--- application_config/sdk_application_config.h
+++ application_config/sdk_application_config.h
@@ -1,11 +1,12 @@
 /** #define sdk / platform dependencies **/
 #include "application_config.h"
 
-#define NRF_SDK14_SPI
-#define NRF_SDK14_YIELD
-#define NRF5_SDK14_PLATFORM
+// Use NRF5 SDK 14
+#define NRF5_SDK14_PLATFORM 1
+#define NRF_SDK14_SPI 1
+#define NRF_SDK14_YIELD 1
 
-#ifdef APPLICATION_NFC
+#if APPLICATION_NFC
   #define NRF5_SDK14_NFC
   #define CLOCK_ENABLED 1 // Required for NFC
   #define NFC_NDEF_MSG_ENABLED 1
@@ -20,7 +21,22 @@
   // <2=> Type 2 Tag 
   // <4=> Type 4 Tag 
   #define NFC_NDEF_MSG_TAG_TYPE 4
-//#define TIMER4 0.     // Timer 4 is reserved by NFC
+  //#define TIMER4 0.     // Timer 4 is reserved by NFC
+  #define NFC_TEXT_BUF_SIZE 128
+  #define NFC_URI_BUF_SIZE 64
+  #define NFC_APP_BUF_SIZE 64
+  #define NFC_DATA_BUF_SIZE 240
+  #define NDEF_FILE_SIZE 512
+  #define NFC_MAX_NUMBER_OF_RECORDS 4
+  #define NFC_TAG_INTERFACE_LOG_ENABLED 1
+  #define NFC_TAG_INTERFACE_LOG_LEVEL   3
+  #define NFC_TAG_INTERFACE_LOG_COLOR   0
+  #define NFC_NDEF_MSG_PARSER_LOG_ENABLED 0
+  #define NFC_NDEF_MSG_PARSER_LOG_LEVEL   3 //INFO
+  #define NFC_NDEF_MSG_PARSER_INFO_COLOR  0 //Black
+  #define NFC_NDEF_RECORD_PARSER_LOG_ENABLED 0
+  #define NFC_NDEF_RECORD_PARSER_LOG_LEVEL 3
+  #define NFC_NDEF_RECORD_PARSER_INFO_COLOR 0
 #endif
 
 // <i> The following modes are supported:
