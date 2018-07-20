344873060ac502825bc803ffc315a2053dcba771
Otso Jousimaa <otso@ojousima.net>
2018-07-20 19:14:26 +0300

    Support low-power sleep

---
+++ application_config/ruuvi_platform_nrf5_sdk15_config.h
@@ -0,0 +1,10 @@
+/**
+ * Configuration for ruuvi.drivers.c nrf5 sdk15 platform support. Enable or disable modules you're using here.
+ *
+ * License: BSD-3
+ * Author: Otso Jousimaa <otso@ojousima.net>
+ */
+
+#define NRF5_SDK15_PLATFORM_ENABLED 1
+#define NRF5_SDK15_GPIO_ENABLED 1
+#define NRF5_SDK15_YIELD_ENABLED 1
--- main.c
+++ main.c
@@ -1,12 +1,38 @@
-#include "yield.h"
+#include "ruuvi_driver_error.h"
+#include "ruuvi_interface_gpio.h"
+#include "ruuvi_interface_yield.h"
+#include "ruuvitag_b.h"
 
 int main(void)
 {
-  // Init yield then, as other init tasks might require delays
-  platform_yield_init();
+  ruuvi_driver_status_t err_code = ruuvi_platform_yield_init();
+
+  // Init GPIO
+  ruuvi_platform_gpio_init();
+
+  // Turn off sensors
+  ruuvi_platform_gpio_configure(SPIM0_SS_ACCELERATION_PIN, RUUVI_INTERFACE_GPIO_MODE_OUTPUT_STANDARD);
+  ruuvi_platform_gpio_set(SPIM0_SS_ACCELERATION_PIN);
+  ruuvi_platform_gpio_configure(SPIM0_SS_ENVIRONMENTAL_PIN, RUUVI_INTERFACE_GPIO_MODE_OUTPUT_STANDARD);
+  ruuvi_platform_gpio_set(SPIM0_SS_ENVIRONMENTAL_PIN);
+  ruuvi_platform_gpio_configure(SPIM0_SCK_PIN, RUUVI_INTERFACE_GPIO_MODE_OUTPUT_STANDARD);
+  ruuvi_platform_gpio_set(SPIM0_SCK_PIN);
+  ruuvi_platform_gpio_configure(SPIM0_MOSI_PIN, RUUVI_INTERFACE_GPIO_MODE_OUTPUT_STANDARD);
+  ruuvi_platform_gpio_set(SPIM0_MOSI_PIN);
+  ruuvi_platform_gpio_configure(SPIM0_MISO_PIN, RUUVI_INTERFACE_GPIO_MODE_OUTPUT_STANDARD);
+  ruuvi_platform_gpio_set(SPIM0_MISO_PIN);
+
+  // LEDs high
+  ruuvi_platform_gpio_configure(LED_RED, RUUVI_INTERFACE_GPIO_MODE_OUTPUT_STANDARD);
+  ruuvi_platform_gpio_set(LED_RED);
+  ruuvi_platform_gpio_configure(LED_GREEN, RUUVI_INTERFACE_GPIO_MODE_OUTPUT_STANDARD);
+  ruuvi_platform_gpio_set(LED_GREEN);
+
+  // Button up
+  ruuvi_platform_gpio_configure(BUTTON_1, RUUVI_INTERFACE_GPIO_MODE_INPUT_PULLUP);
 
   while (1)
   {
-    platform_yield();
+    ruuvi_platform_yield();
   }
 }
