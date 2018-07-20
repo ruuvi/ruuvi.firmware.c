4ba2811d9146f89b1952d5588c51d6399832fa84
Otso Jousimaa <otso@ojousima.net>
2018-07-20 16:12:47 +0300

    clean up for ruuviblog

--- tasks/task_led.h
+++
@@ -1,8 +0,0 @@
-#ifndef TASK_LEDS_H
-#define TASK_LEDS_H
-#include "ruuvi_error.h"
-
-ruuvi_status_t task_led_init(void);
-ruuvi_endpoint_status_t led_handler(ruuvi_standard_message_t* const message);
-
-#endif
--- tasks/task_spi.h
+++
@@ -1,6 +0,0 @@
-#ifndef TASK_SPI_H
-#define TASK_SPI_H
-
-#include "ruuvi_error.h"
-ruuvi_status_t task_spi_init(void);
-#endif
