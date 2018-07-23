f421b42459099485b7a9e572a6e63e230eaa9499
Otso Jousimaa <otso@ojousima.net>
2018-07-23 12:14:40 +0300

    Remove extra ignore, add missing file

---
+++ application_config/nrf5_sdk15_application_config.h
@@ -0,0 +1,7 @@
+/**
+ * Configuration for ruuvi.drivers.c nrf5 sdk15 sdk_config. Platform has default configuration, you'll need to only define the overrides.
+ *
+ * License: BSD-3
+ * Author: Otso Jousimaa <otso@ojousima.net>
+ */
+
