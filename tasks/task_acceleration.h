db68dd09459b8dd42e87e1c4f138476359196df3
Otso Jousimaa <otso@ojousima.net>
2018-09-04 12:26:22 +0300

    Fix comments, includes

--- tasks/task_acceleration.h
+++ tasks/task_acceleration.h
@@ -11,20 +11,17 @@
 #include "ruuvi_boards.h"
 #include "ruuvi_driver_error.h"
 #include "ruuvi_driver_sensor.h"
-#include "ruuvi_interface_bme280.h"
-#include "ruuvi_interface_environmental.h"
-#include "ruuvi_interface_environmental_mcu.h"
 #include "ruuvi_interface_log.h"
 
 /**
- * Auto-detects and initializes environmental sensor in low-power state
+ * Auto-detects and initializes acceleration sensor in low-power state
  * Reads supported sensors from board.h and tries to initialize them.
  * Configures the sensor according to defaults in application_config.h
  */
 ruuvi_driver_status_t task_acceleration_init(void);
 
 /**
- * Prints environmental data to log at given severity
+ * Prints Acceleration data to log at given severity
  */
 ruuvi_driver_status_t task_acceleration_data_log(const ruuvi_interface_log_severity_t level);
 
