59560fd94e5940ee3771da6fa69c4d999e408436
Otso Jousimaa <otso@ojousima.net>
2018-09-27 22:15:55 +0300

    Support ARMGCC

--- tasks/task_nfc.c
+++ tasks/task_nfc.c
@@ -16,13 +16,13 @@ ruuvi_driver_status_t task_nfc_init(void)
   ruuvi_driver_status_t err_code = RUUVI_DRIVER_SUCCESS;
   int written = 0;
   uint8_t fw_prefix[] = {'F', 'W', ':', ' '};
-  char version_string[APPLICATION_COMMUNICATION_NFC_TEXT_BUFFER_SIZE] = { 0 };
+  uint8_t version_string[APPLICATION_COMMUNICATION_NFC_TEXT_BUFFER_SIZE] = { 0 };
   memcpy(version_string, fw_prefix, sizeof(fw_prefix));
-  written = snprintf(version_string + sizeof(fw_prefix),
+  written = snprintf((char*)(version_string + sizeof(fw_prefix)),
                      APPLICATION_COMMUNICATION_NFC_TEXT_BUFFER_SIZE - sizeof(fw_prefix),
                      "%s", APPLICATION_FW_VERSION);
   if(!(written > 0 && APPLICATION_COMMUNICATION_NFC_TEXT_BUFFER_SIZE > written)) { err_code |= RUUVI_DRIVER_ERROR_DATA_SIZE; }
-  err_code |= ruuvi_interface_communication_nfc_fw_version_set(version_string, strlen(version_string));
+  err_code |= ruuvi_interface_communication_nfc_fw_version_set(version_string, strlen((char*)version_string));
 
   uint64_t mac = 0;
   err_code |=  ruuvi_interface_communication_radio_address_get(&mac);
@@ -34,28 +34,30 @@ ruuvi_driver_status_t task_nfc_init(void)
   mac_buffer[4] = (mac >> 8) & 0xFF;
   mac_buffer[5] = (mac >> 0) & 0xFF;
   //8 hex bytes
-  char name[APPLICATION_COMMUNICATION_NFC_TEXT_BUFFER_SIZE] = { 0 };
-  written = snprintf(name,
+  uint8_t name[APPLICATION_COMMUNICATION_NFC_TEXT_BUFFER_SIZE] = { 0 };
+  written = snprintf((char*)name,
                      APPLICATION_COMMUNICATION_NFC_TEXT_BUFFER_SIZE,
                      "MAC: %02X:%02X:%02X:%02X:%02X:%02X",
                      mac_buffer[0], mac_buffer[1], mac_buffer[2], mac_buffer[3], mac_buffer[4], mac_buffer[5]);
   if(!(written > 0 && APPLICATION_COMMUNICATION_NFC_TEXT_BUFFER_SIZE > written)) { err_code |= RUUVI_DRIVER_ERROR_DATA_SIZE; }
-  err_code |= ruuvi_interface_communication_nfc_address_set(name, strlen(name));
+  err_code |= ruuvi_interface_communication_nfc_address_set(name, strlen((char*)name));
 
   uint64_t id = 0;
   err_code |= ruuvi_interface_communication_id_get(&id);
   uint8_t prefix[] = {'I', 'D', ':', ' '};
-  char id_string[APPLICATION_COMMUNICATION_NFC_TEXT_BUFFER_SIZE] = { 0 };
+  uint8_t id_string[APPLICATION_COMMUNICATION_NFC_TEXT_BUFFER_SIZE] = { 0 };
   memcpy(id_string, prefix, sizeof(prefix));
   uint32_t id0 = (id>>32) & 0xFFFFFFFF;
   uint32_t id1 = id & 0xFFFFFFFF;
-  written = snprintf(id_string + sizeof(prefix),
+  written = snprintf((char*)(id_string + sizeof(prefix)),
                      APPLICATION_COMMUNICATION_NFC_TEXT_BUFFER_SIZE - sizeof(prefix),
                      "%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X",
-                     (id0>>24)&0xFF, (id0>>16)&0xFF, (id0>>8)&0xFF, id0&0xFF,
-                     (id1>>24)&0xFF, (id1>>16)&0xFF, (id1>>8)&0xFF, id1&0xFF);
+                     (unsigned int)(id0>>24)&0xFF, (unsigned int)(id0>>16)&0xFF,
+                     (unsigned int)(id0>>8)&0xFF, (unsigned int)id0&0xFF,
+                     (unsigned int)(id1>>24)&0xFF, (unsigned int)(id1>>16)&0xFF,
+                     (unsigned int)(id1>>8)&0xFF, (unsigned int)id1&0xFF);
   if(!(written > 0 && APPLICATION_COMMUNICATION_NFC_TEXT_BUFFER_SIZE > written)) { err_code |= RUUVI_DRIVER_ERROR_DATA_SIZE; }
-  err_code |= ruuvi_interface_communication_nfc_id_set(id_string, strlen(id_string));
+  err_code |= ruuvi_interface_communication_nfc_id_set(id_string, strlen((char*)id_string));
   err_code |= ruuvi_interface_communication_nfc_init(&channel);
   err_code |= ruuvi_interface_communication_nfc_data_set(); // Call this to setup data to buffers
   return err_code;
