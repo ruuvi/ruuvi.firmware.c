36568ca3fe96da116e3b3922b18beea40eb0db1e
Otso Jousimaa <otso@ojousima.net>
2019-04-27 12:38:19 +0300

    Add comments to configuration file

--- application_config/application_config.h
+++ application_config/application_config.h
@@ -9,24 +9,29 @@
 #define APPLICATION_CONFIG_H
 #include "application_modes.h" // Includes different modes, such as long-life with low sampling rate and tx rate.
 
-#define APPLICATION_FW_VERSION "RuuviFW 3.17.0"
+/** @brief Version string, displayed in NFC read and GATT data on DIS */
+#define APPLICATION_FW_VERSION "RuuviFW 3.18.0"
 
-// Pick a power of 2 for nRF5 backend. 128 is recommended.
-#define APPLICATION_LOG_BUFFER_SIZE              256
+/** @brief Bytes of RAM to conserve for printed log messages
+ *  Pick a power of 2 for nRF5 backend. at least 128 is recommended.
+ */
+#define APPLICATION_LOG_BUFFER_SIZE           256
 
-// Use nRF5 SDK15
-#define NRF5_SDK15_PLATFORM_ENABLED              1
+/** 
+ * @brief enable compilation of NRF5 SDK15 implementation of driver interface functions.
+ * Only one implementation can be enabled at a time.
+ */
+#define RUUVI_NRF5_SDK15_ENABLED              1
 
 /**
- * Data format configuration
+ * @brief Battery voltage measurement mode configuration
  */
-// Voltage mode - select one
 #ifndef APPLICATION_BATTERY_VOLTAGE_MODE
-#define APPLICATION_BATTERY_VOLTAGE_SIMPLE   0  // Simple mode: Not synchronized to anything, sampled at regular interval
-#define APPLICATION_BATTERY_VOLTAGE_RADIO    1  // Radio mode: Voltage is sampled after radio tx, refreshed after interval has passed.
-#define APPLICATION_BATTERY_VOLTAGE_DROOP    0  // Droop mode: Battery is read after TX and after a brief recovery period. Droop is reported
+#define APPLICATION_BATTERY_VOLTAGE_SIMPLE   0  //<! Simple mode: Not synchronized to anything, sampled at regular interval
+#define APPLICATION_BATTERY_VOLTAGE_RADIO    1  //<! Radio mode: Voltage is sampled after radio tx, refreshed after interval has passed.
+#define APPLICATION_BATTERY_VOLTAGE_DROOP    0  //<! Droop mode: Battery is read after TX and after a brief recovery period. Droop is reported
 #endif
-#define APPLICATION_BATTERY_DROOP_DELAY_MS   2  // Milliseconds between active and recovered tx
+#define APPLICATION_BATTERY_DROOP_DELAY_MS   2  //<! Milliseconds between active and recovered tx
 #if(APPLICATION_BATTERY_VOLTAGE_SIMPLE + APPLICATION_BATTERY_VOLTAGE_RADIO + APPLICATION_BATTERY_VOLTAGE_DROOP != 1)
   #error "Select application battery voltage monitor mode by defining one constant as 1"
 #endif
@@ -34,56 +39,114 @@
 /**
  * Environmental sensor configuration
  **/
-// Sample rate is in Hz. This configures only the sensor, not transmission rate of data.
 #ifndef APPLICATION_ENVIRONMENTAL_CONFIGURED
+  /** @brief sample rate, in Hz. */
   #define APPLICATION_ENVIRONMENTAL_SAMPLERATE RUUVI_DRIVER_SENSOR_CFG_MIN
 
-  // Resolution and scale cannot be adjusted on BME280
+  /** @brief BME280 cannot adjust resolution, use default */
   #define APPLICATION_ENVIRONMENTAL_RESOLUTION RUUVI_DRIVER_SENSOR_CFG_DEFAULT
+
+  /** @brief BME280 cannot adjust scale, use default */
   #define APPLICATION_ENVIRONMENTAL_SCALE      RUUVI_DRIVER_SENSOR_CFG_DEFAULT
 
-  // Valid values for BME280 are: (RUUVI_DRIVER_SENSOR_DSP_)LAST, IIR, OS
-  // IIR slows step response but lowers noise
-  // OS increases power consumption but lowers noise.
-  // See https://blog.ruuvi.com/humidity-sensor-673c5b7636fc and https://blog.ruuvi.com/dsp-compromises-3f264a6b6344
-  #define APPLICATION_ENVIRONMENTAL_DSPFUNC    RUUVI_DRIVER_SENSOR_DSP_IIR
+  /** @brief DSP configuration of environmental sensor. 
+   * Valid values for BME280 are: (RUUVI_DRIVER_SENSOR_DSP_)LAST, LOW_PASS, OS
+   * Low pass slows step response but lowers noise
+   * Ooversampling (OS) increases power consumption but lowers noise.
+   * @see https://blog.ruuvi.com/humidity-sensor-673c5b7636fc and https://blog.ruuvi.com/dsp-compromises-3f264a6b6344
+   */
+  #define APPLICATION_ENVIRONMENTAL_DSPFUNC    RUUVI_DRIVER_SENSOR_DSP_LOW_PASS
 
-  // No effect on _LAST, use 1. On _OS and _IIR valid values are 2, 4, 8 and 16.
+  /** @brief Parameter to DSP function.
+   * The parameter affects how agressively the DSP is applied, higher means 
+   * stronger effect. For example Oversampling with parameter 8 means 8 samples are averaged for one sample.
+   *
+   * Valid values are RUUVI_DRIVER_SENSOR_CFG_MAX, RUUVI_DRIVER_SENSOR_CFG_MIN, RUUVI_DRIVER_SENSOR_CFG_DEFAULT.
+   * 1, 2, 4, 8, 16
+   */
   #define APPLICATION_ENVIRONMENTAL_DSPPARAM   RUUVI_DRIVER_SENSOR_CFG_MAX
 
-  // (RUUVI_DRIVER_SENSOR_CFG_)SLEEP, SINGLE or CONTINUOUS
+  /**
+   * @brief default mode of environmental sensor. 
+   * Valid values are RUUVI_DRIVER_SENSOR_CFG_SLEEP, RUUVI_DRIVER_SENSOR_CFG_SINGLE and RUUVI_DRIVER_SENSOR_CFG_CONTINUOUS.
+   * Sleep enters lowest-power mode possible. 
+   * Single commands sensor to take a new sample, waits sample to be available and then returns the data from sensor.
+   * Continuous keeps the sensor running on the background regardless of how often data is read.
+   * Continuous is recommended mode for most applications.
+   */
   #define APPLICATION_ENVIRONMENTAL_MODE       RUUVI_DRIVER_SENSOR_CFG_CONTINUOUS
 #endif
 
 
-// Allow BME280 support compilation
+/** @brief Enable compiling BME280 interface functions */
 #define RUUVI_INTERFACE_ENVIRONMENTAL_BME280_ENABLED 1
+
+/** @brief Enable compiling BME280 SPI interface */
+#define RUUVI_INTERFACE_ENVIRONMENTAL_BME280_SPI_ENABLED 1
+
+/** @brief Enable compiling BME280 I2C interface */
+#define RUUVI_INTERFACE_ENVIRONMENTAL_BME280_I2C_ENABLED 0
 
 /**
  * Accelerometer configuration
  **/
- #ifndef APPLICATION_ACCELERATION_CONFIGURED
-  // 1, 10, 25, 50, 100, 200 for LIS2DH12
-  #define APPLICATION_ACCELEROMETER_SAMPLERATE 10
+#ifndef APPLICATION_ACCELERATION_CONFIGURED
+  /** @brief sample rate of accelerometer
+   *
+   * This controls only internal sampling of accelerometer, not reading frequency of data.
+   * Valid values are 1, 10, 25, 50, 100, 200 for LIS2DH12
+   * @c RUUVI_DRIVER_CFG_MIN, @c RUUVI_DRIVER_CFG_MAX, and @c RUUVI_DRIVER_CFG_DEFAULT, are valid for all sensors.
+   */
+  #define APPLICATION_ACCELEROMETER_SAMPLERATE 1
 
-  // 8, 10, 12 for LIS2DH12
-  #define APPLICATION_ACCELEROMETER_RESOLUTION 10
+  /** @brief Resolution of accelerometer, in bits.
+   *
+   * This controls only internal sampling of accelerometer, not reading frequency of data.
+   * Valid values are  8, 10, 12 for LIS2DH12
+   * @c RUUVI_DRIVER_CFG_MIN, @c RUUVI_DRIVER_CFG_MAX, and @c RUUVI_DRIVER_CFG_DEFAULT, are valid for all sensors.
+   */
+  #define APPLICATION_ACCELEROMETER_RESOLUTION RUUVI_DRIVER_CFG_DEFAULT
 
-  // 2, 4, 8, 16 for LIS2DH12
+  /** @brief Scale of accelerometer, in G (9.81 m / s^2).
+   *
+   * This controls only internal sampling of accelerometer, not reading frequency of data.
+   * Valid values are  2, 4, 8, 16 for LIS2DH12
+   * @c RUUVI_DRIVER_CFG_MIN, @c RUUVI_DRIVER_CFG_MAX, and @c RUUVI_DRIVER_CFG_DEFAULT, are valid for all sensors.
+   */
   #define APPLICATION_ACCELEROMETER_SCALE   RUUVI_DRIVER_SENSOR_CFG_MIN
 
-  // LAST or HIGH_PASS
-  #define APPLICATION_ACCELEROMETER_DSPFUNC RUUVI_DRIVER_SENSOR_DSP_LAST
-  #define APPLICATION_ACCELEROMETER_DSPPARAM 1
+  /** @brief DPS function for accelerometer
+   *  Enable high-pass to filter out the gravity. 
+   */
+  #define APPLICATION_ACCELEROMETER_DSPFUNC RUUVI_DRIVER_SENSOR_DSP_HIGH_PASS
 
-  // SLEEP or CONTINUOUS
+  /** @brief DPS parameter for accelerometer
+   *  Higher is more agressive. 
+   *  Valid values are  1, 2, 3, 4 for LIS2DH12
+   *  @c RUUVI_DRIVER_CFG_MIN, @c RUUVI_DRIVER_CFG_MAX, and @c RUUVI_DRIVER_CFG_DEFAULT, are valid for all sensors.
+   */
+  #define APPLICATION_ACCELEROMETER_DSPPARAM RUUVI_DRIVER_CFG_DEFAULT
+
+  /** @brief Operating mode for accelerometer
+   * Valid values are RUUVI_DRIVER_SENSOR_CFG_SLEEP, RUUVI_DRIVER_SENSOR_CFG_SINGLE and RUUVI_DRIVER_SENSOR_CFG_CONTINUOUS.
+   * Sleep enters lowest-power mode possible. 
+   * Single commands sensor to take a new sample, waits sample to be available and then returns the data from sensor.
+   * Continuous keeps the sensor running on the background regardless of how often data is read.
+   * Continuous is recommended mode for most applications.
+   */
   #define APPLICATION_ACCELEROMETER_MODE RUUVI_DRIVER_SENSOR_CFG_CONTINUOUS
 
-  // Up to scale
+  /**
+   * @brief activity interrupt threshold for accelerometer in G.
+   * Maximum is the scale of accelerometer.
+   */
   #define APPLICATION_ACCELEROMETER_ACTIVITY_THRESHOLD 0.100f
 #endif
 
-// Allow LIS2DH12 support compilation
+/** @brief enable compiling accelerometer interface */
+#define RUUVI_INTERFACE_ACCELERATION_ENABLED 1
+
+/** @brief Enable compiling lis2dh12 accelerometer support */
 #define RUUVI_INTERFACE_ACCELERATION_LIS2DH12_ENABLED 1
 
 /**
@@ -95,18 +158,18 @@
 #define APPLICATION_ADC_DSPFUNC            RUUVI_DRIVER_SENSOR_DSP_LAST
 #define APPLICATION_ADC_DSPPARAM           1
 #define APPLICATION_ADC_MODE               RUUVI_DRIVER_SENSOR_CFG_SINGLE // Note: call to task_adc_sample will leave the ADC in single-shot mode. Use data get only in continuous mode
-#define APPLICATION_ADC_SAMPLE_INTERVAL_MS 30000 // Valid for single mode
+#define APPLICATION_ADC_SAMPLE_INTERVAL_MS 3000 // Valid for single mode
 
 /**
  * Bluetooth configuration
- *
  */
 // Avoid "even" values such as 100 or 1000 to eventually drift apart from the devices transmitting at same interval
 #ifndef APPLICATION_ADVERTISING_CONFIGURED
-  #define APPLICATION_ADVERTISING_INTERVAL              1010
-  #define APPLICATION_CONNECTION_ADVERTISEMENT_INTERVAL APPLICATION_ADVERTISING_INTERVAL
+  #define APPLICATION_ADVERTISING_INTERVAL              (1000 * APPLICATION_ACCELEROMETER_DATASETS * 32 / 400) 
+  #define APPLICATION_CONNECTION_ADVERTISEMENT_INTERVAL 100
   #define APPLICATION_ADVERTISING_POWER                 RUUVI_BOARD_TX_POWER_MAX
-  #define APPLICATION_DATA_FORMAT                       5
+  #define APPLICATION_DATA_FORMAT                       0x03
+  #define APPLICATION_STANDBY_INTERVAL                  9900
 #endif
 
 // Apple connection parameter quidelines:
@@ -117,14 +180,14 @@
 // Interval Max * (slave latency + 1) <= 2 seconds
 // Interval MAX + (slave latency +1) * 3 < connSupervisionTimeout
 #define APPLICATION_GATT_CONN_INTERVAL_MIN_MS      15
-#define APPLICATION_GATT_CONN_INTERVAL_MAX_MS      45
-#define APPLICATION_GATT_CONN_SLAVE_SKIP_INTERVALS 30   // Slave latency. How many intervals can be skipped. 31 * 45 < 2000
+#define APPLICATION_GATT_CONN_INTERVAL_MAX_MS      15
+#define APPLICATION_GATT_CONN_SLAVE_SKIP_INTERVALS 31   // Slave latency. How many intervals can be skipped. 31 * 45 < 2000
 #define APPLICATION_GATT_CONN_TIMEOUT_MS           5600 // 31 * 45 * 3 < 5600
 
 /**
  * NFC configuration
  */
-// Longest text in a text field, i.e. "FW: ruuvi.firmware.c 3.10.0
+// Longest text in a text field, i.e. "FW: ojousima.logger.c 3.10.0"
 #define APPLICATION_COMMUNICATION_NFC_TEXT_BUFFER_SIZE (32)
 // Longest binary message
 #define APPLICATION_COMMUNICATION_NFC_DATA_BUFFER_SIZE APPLICATION_COMMUNICATION_NFC_TEXT_BUFFER_SIZE
@@ -138,7 +201,7 @@
  * Task scheduler configuration
  */
 #define APPLICATION_TASK_DATA_MAX_SIZE 0
-#define APPLICATION_TASK_QUEUE_MAX_LENGTH 10
+#define APPLICATION_TASK_QUEUE_MAX_LENGTH 20
 
 /**
  * Flags which determine which c-modules are compiled in.
@@ -155,11 +218,13 @@
 #define APPLICATION_GPIO_INTERRUPT_ENABLED          1
 #define APPLICATION_ENVIRONMENTAL_MCU_ENABLED       1
 #define APPLICATION_ENVIRONMENTAL_BME280_ENABLED    1
+#define APPLICATION_I2C_ENABLED                     1
 #define APPLICATION_POWER_ENABLED                   1
 #define APPLICATION_RTC_MCU_ENABLED                 1
 #define APPLICATION_SCHEDULER_ENABLED               1
 #define APPLICATION_SPI_ENABLED                     1
 #define APPLICATION_TIMER_ENABLED                   1
+#define APPLICATION_TIMER_MAX_INSTANCES             10 ///< Timers are allocated statically on RAM
 #define APPLICATION_WATCHDOG_ENABLED                1
 #define APPLICATION_WATCHDOG_INTERVAL_MS            120000u
 #define APPLICATION_YIELD_ENABLED                   1
@@ -172,6 +237,7 @@
 #define APPLICATION_LOG_BACKEND_RTT_ENABLED         1
 //#define APPLICATION_LOG_BACKEND_UART_ENABLED        0 // UART not implemented
 
-#define  APPLICATION_FLASH_DATA_PAGES_NUMBER        20
+// 64 * 1024 words (4096 bytyes) = 262144 bytes = 256 kB.
+#define  APPLICATION_FLASH_DATA_PAGES_NUMBER        64
 
 #endif
