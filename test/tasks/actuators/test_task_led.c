0fba9fa70dfd62782d68879565a64292253480ae
Otso Jousimaa <otso@ojousima.net>
2019-11-14 19:15:35 +0200

    Ceedling integratd

---
+++ test/tasks/actuators/test_task_led.c
@@ -0,0 +1,16 @@
+#include "unity.h"
+#include "mock_ruuvi_interface_gpio.h"
+#include "task_led.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_task_led_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement task_led");
+}
