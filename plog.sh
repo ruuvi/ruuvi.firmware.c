c44167d786c1fe1f9574c96a53f42a731f1bec17
Otso Jousimaa <otso@ojousima.net>
2019-11-14 18:05:56 +0200

    Remove nRF SDK, GCC files from static analysis

--- plog.sh
+++ plog.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-rm -rf issues/*
-plog-converter -a 'GA:1,2;64:1;OP:1,2,3;CS:1;MISRA:1,2' -d V677,Renew -t fullhtml -o issues _build/nrf52832_xxaa/*.PVS-Studio.log
-plog-converter -a 'GA:1,2;64:1;OP:1,2,3;CS:1;MISRA:1,2' -d V677,Renew -t tasklist -o issues/PVS-Studio.tasks _build/nrf52832_xxaa/*.PVS-Studio.log
-plog-converter -a 'GA:1,2;64:1;OP:1,2,3;CS:1;MISRA:1,2' -d V677,Renew -t xml -o      issues/pvs.xml _build/nrf52832_xxaa/*.PVS-Studio.log
+rm -rf issues/fullhtml
+rm issues/shorthtml.html
+rm issues/PVS-Studio.tasks
+rm issues/pvs.xml
+plog-converter -a 'GA:1,2;64:1;OP:1,2,3;CS:1;MISRA:1,2' -d Renew -t fullhtml -o issues issues/raw/*.PVS-Studio.log
+plog-converter -a 'GA:1,2;64:1;OP:1,2,3;CS:1;MISRA:1,2' -d Renew -t tasklist -o issues/PVS-Studio.tasks issues/raw/*.PVS-Studio.log
+plog-converter -a 'GA:1,2;64:1;OP:1,2,3;CS:1;MISRA:1,2' -d Renew -t xml -o      issues/pvs.xml issues/raw/*.PVS-Studio.log
+plog-converter -a 'GA:1,2;64:1;OP:1,2,3;CS:1;MISRA:1,2' -d Renew -t html -o     issues/shorthtml.html issues/raw/*.PVS-Studio.log
