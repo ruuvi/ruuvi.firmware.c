75369f1bf64e4c0f8edb4afc84add3f2d46a2c5d
Otso Jousimaa <otso@ojousima.net>
2019-12-26 17:30:52 +0200

    Test button

--- src/tasks/sensors/task_button.c
+++ src/tasks/sensors/task_button.c
@@ -1,73 +1,62 @@
+/**
+ * @addtogroup button_tasks
+ */
+/*@{*/
+/**
+ * @file task_button.c
+ * @author Otso Jousimaa <otso@ojousima.net>
+ * @date 2019-12-26
+ * @copyright Ruuvi Innovations Ltd, license BSD-3-Clause.
+ *
+ *
+ */
+
 #include "ruuvi_driver_error.h"
 #include "ruuvi_boards.h"
+#include "application_config.h"
 #include "ruuvi_interface_gpio.h"
-#include "ruuvi_interface_rtc.h"
-#include "ruuvi_interface_scheduler.h"
-#include "ruuvi_interface_watchdog.h"
-#include "ruuvi_interface_yield.h"
 #include "ruuvi_interface_log.h"
-#include "task_acceleration.h"
-#include "task_adc.h"
-#include "task_advertisement.h"
 #include "task_button.h"
-#include "task_environmental.h"
-#include "task_led.h"
-#include "task_rtc.h"
+
 #include <stddef.h>
+
 #if APPLICATION_BUTTON_ENABLED
 static task_button_fp_t button_callback = NULL;
 
 // Wrapper to gpio interrupt function
-static void on_button (ruuvi_interface_gpio_evt_t event)
+#ifndef CEEDLING
+static 
+#endif
+void on_button_isr (ruuvi_interface_gpio_evt_t event)
 {
     if (NULL != button_callback)
     {
-        button_callback();
+        button_callback(event);
     }
 
     ruuvi_interface_log (RUUVI_INTERFACE_LOG_INFO, "Button\r\n");
 }
 
-ruuvi_driver_status_t task_button_init (ruuvi_interface_gpio_slope_t slope,
-                                        task_button_fp_t action)
+ruuvi_driver_status_t task_button_init (task_button_fp_t action)
 {
     ruuvi_driver_status_t err_code = RUUVI_DRIVER_SUCCESS;
     button_callback = action;
     ruuvi_interface_gpio_id_t pin = {.pin = RUUVI_BOARD_BUTTON_1};
+    ruuvi_interface_gpio_slope_t slope = RUUVI_INTERFACE_GPIO_SLOPE_TOGGLE;
+    ruuvi_interface_gpio_mode_t  mode  = RUUVI_INTERFACE_GPIO_MODE_INPUT_PULLDOWN;
+    if(RUUVI_INTERFACE_GPIO_LOW == RUUVI_BOARD_BUTTONS_ACTIVE_STATE)
+    {
+    mode  = RUUVI_INTERFACE_GPIO_MODE_INPUT_PULLUP;        
+    }
     err_code |= ruuvi_interface_gpio_interrupt_enable (pin, slope,
-                RUUVI_INTERFACE_GPIO_MODE_INPUT_PULLUP, on_button);
+                mode, &on_button_isr);
     return err_code;
 }
 
-ruuvi_driver_status_t task_button_on_press (void)
-{
-    static uint64_t last_press = 0;
-    // returns UINT64_MAX if RTC is not running.
-    uint64_t now = task_rtc_millis();
-    ruuvi_driver_status_t err_code = RUUVI_DRIVER_SUCCESS;
-
-    // Debounce button
-    if ( (now - last_press) > RUUVI_BOARD_BUTTON_DEBOUNCE_PERIOD_MS)
-    {
-        // Do your button action here
-        // p_event_data points to action data,
-        // event size is size of action data at most equal to APPLICATION_TASK_DATA_MAX_SIZE
-        // handler is function pointer which will handle this event
-        //ruuvi_interface_scheduler_event_put (void const *p_event_data, uint16_t event_size, ruuvi_scheduler_event_handler_t handler);
-    }
-
-    // store time of press for debouncing if possible
-    if (RUUVI_DRIVER_UINT64_INVALID != now)
-    {
-        last_press = now;
-    }
-
-    return err_code;
-}
-#else
-ruuvi_driver_status_t task_button_init (ruuvi_interface_gpio_slope_t slope,
-                                        task_button_fp_t action)
+#else // APPLICATION_BUTTON_ENABLED
+ruuvi_driver_status_t task_button_init (task_button_fp_t action)
 {
     return RUUVI_DRIVER_SUCCESS;
 }
 #endif
+/*@}*/
--- src/tasks/sensors/task_button.h
+++ src/tasks/sensors/task_button.h
@@ -1,10 +1,22 @@
 /**
- * Ruuvi Firmware 3.x Button tasks.
+ * @defgroup advertisement_tasks Advertisement tasks
+ * @brief Bluetooth Low Energy advertising
  *
- * License: BSD-3
- * Author: Otso Jousimaa <otso@ojousima.net>
- **/
-
+ */
+/*@{*/
+/**
+ * @defgroup button_tasks  Button functionality
+ */
+/*@}*/
+/*@{*/
+/**
+ * @file task_button.h
+ * @author Otso Jousimaa <otso@ojousima.net>
+ * @date 2019-12-26
+ * @copyright Ruuvi Innovations Ltd, license BSD-3-Clause.
+ *
+ *
+ */
 #ifndef  TASK_BUTTON_H
 #define  TASK_BUTTON_H
 
@@ -12,24 +24,25 @@
 #include "ruuvi_driver_error.h"
 #include "ruuvi_interface_gpio_interrupt.h"
 
-typedef ruuvi_driver_status_t (*task_button_fp_t) (void);
+/** @brief Called on button event with the slope of edge */
+typedef void (*task_button_fp_t) (const ruuvi_interface_gpio_evt_t event);
 
 /**
- * Button initialization function. Configures GPIO as pulled-up interrupt input.
+ * @brief Button initialization function.
  *
- * parameter slope: slopes to interrupt on. (RUUVI_INTERFACE_GPIO_SLOPE_)HITOLO, LOTOHI or TOGGLE
- * parameter action: Function to be called when button interrupt occurs
+ * Requires GPIO and interrupts to be initialized.
+ * Configures GPIO as pullup/-down according to button active state in ruuvi_boards.h
  *
- * return: Status code from the stack. RUUVI_DRIVER_SUCCESS if no errors occured.
+ * @param[in] action: Function to be called when button interrupt occurs
+ *
+ * @return Status code from the stack. RUUVI_DRIVER_SUCCESS if no errors occured.
  **/
-ruuvi_driver_status_t task_button_init (ruuvi_interface_gpio_slope_t slope,
-                                        task_button_fp_t action);
+ruuvi_driver_status_t task_button_init (task_button_fp_t action);
 
-/**
- * Calls other Button tasks.
- *
- * return: Status code from the stack. RUUVI_DRIVER_SUCCESS if no errors occured.
- */
-ruuvi_driver_status_t task_button_on_press (void);
+#ifdef CEEDLING
+/** @brief Provide Ceedling a way to trigger interrupt */
+void on_button_isr (ruuvi_interface_gpio_evt_t event);
+#endif
 
+/*@}*/
 #endif
