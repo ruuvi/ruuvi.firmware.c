2e9ead9ed3c4ab93bad9ad1835df6494bd086dd6
Otso Jousimaa <otso@ojousima.net>
2020-01-10 20:17:45 +0200

    Run automatic update test

--- system_tests/test_sdk_dfu.sh
+++ system_tests/test_sdk_dfu.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 source .test_env
 SDK_UPDATE="ruuvitag_b_armgcc_ruuvifw_test_v3.28.13_sdk12.3_to_15.3_dfu.zip"
 APP_UPDATE="ruuvitag_b_armgcc_ruuvifw_test_v3.28.13_dfu_app.zip"
-RESULT_FILE="sdk_dfu_test.txt" 
+RESULT_FILE="test_sdk_update_result.txt" 
 TEST_FILE="test_sdk_update.xml"
 
 fetch_result () {
@@ -25,11 +25,8 @@ fi
 adb push ${SDK_UPDATE} "/sdcard/${FIRMWARE_PATH}/${SDK_UPDATE}"
 adb push ${APP_UPDATE} "/sdcard/${FIRMWARE_PATH}/${APP_UPDATE}"
 adb push ${TEST_FILE} "/sdcard/${FIRMWARE_PATH}/${TEST_FILE}" #> /dev/null
-RESULT=$? #> /dev/null
 adb shell rm "/sdcard/${FIRMWARE_PATH}/${RESULT_FILE}" #> /dev/null
-RESULT=${RESULT}+$? #> /dev/null
-
-RESULT=${RESULT}+$? #> /dev/null
+rm ${RESULT_FILE}
 adb shell am start-foreground-service -a no.nordicsemi.android.action.START_TEST \
     -e FIRMWARE_PATH "/${FIRMWARE_PATH}" -e SDK_UPDATE ${SDK_UPDATE} \
     -e APP_UPDATE ${APP_UPDATE} \
