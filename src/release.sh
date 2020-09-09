306827ae72bb0378385c6fcdf8eae41975a5f5e4
Otso Jousimaa <otso@ojousima.net>
2020-09-09 22:07:37 +0300

    Fix ID parsing from commit creation

--- src/release.sh
+++ src/release.sh
@@ -5,25 +5,22 @@ GH_REPO="ruuvi.firmware.c"
 
 BOARDS=(ruuvitag_b kaarle kalervo keijo)
 VARIANTS=(default longlife longmem test)
-TAG=$(eval git describe --tags --exact-match)
+TAG=$(git describe --tags --exact-match)
 if [ -z "$TAG" ]; then echo "Tag is not set, exit"; exit; fi
 
-message=$(eval "git for-each-ref refs/tags/${TAG} --format='\%(contents)'")
-#echo ${message}
+message="$(git for-each-ref refs/tags/$TAG --format='%(contents)')"
 
 # Get the title and the description as separated variables
-name=$(eval echo "${message}" | head -n1)
-#echo ${name}
-description=$(eval echo ${message} | tail -n +3)
-#description=$(eval echo "${description}" | sed 's/\n/\\n/g') # Escape line breaks to prevent json parsing problems
-#echo ${description}  
+name=$(echo "$message" | head -n 1)
+description=$(echo "$message" | tail -n -1)
 # Create a release
-release=curl -XPOST -H "Authorization:token ${GH_TOKEN}" --data "{\"tag_name\": \"${TAG}\", \"target_commitish\": \"master\", \"name\": \"$name\", \"body\": \"${description}\", \"draft\": false, \"prerelease\": true}" https://api.github.com/repos/${GH_USER}/${GH_REPO}/releases
-#echo ${release}
+release=$(curl -XPOST -H "Authorization:token ${GH_TOKEN}" --data "{\"tag_name\": \"${TAG}\", \"target_commitish\": \"master\", \"name\": \"$name\", \"body\": \"${description}\", \"draft\": false, \"prerelease\": true}" https://api.github.com/repos/${GH_USER}/${GH_REPO}/releases)
+echo "${release}"
 
 # Extract the id of the release from the creation response
-id=$(eval echo "${release}" |  awk -F'[, \t]*' '/"id":/{print $3}' | head -1)
-echo ${id}
+id=$(cat release_output | grep -m 1 "id.:" | grep -Eo '[0-9]+')
+[ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$release" | awk 'length($0)<100' >&2; exit 1; }
+echo "${id}"
 
 for BOARD in "${BOARDS[@]}"
 do
@@ -41,3 +38,4 @@ do
 
   done
 done
+
