ee2f7281ba6b8057d656d0b42b2daf3b7d052dac
Dennis German <DGermang@Real-World-Systems.com>
2021-03-01 23:35:15 -0500

    Add printing version
    init_log and log message including APP_FW_VERSION, compiled date& time .
    
    Will add MAC later.

--- src/main.c
+++ src/main.c
@@ -29,7 +29,7 @@
 #include "ruuvi_task_flash.h"
 #include "ruuvi_task_gpio.h"
 #include "ruuvi_task_led.h"
-#include "nrf52.h"
+
 static inline void LOG   (const char *const msg) { ri_log (RI_LOG_LEVEL_INFO,    msg);}
 
 #if (!RUUVI_RUN_TESTS)
@@ -110,10 +110,8 @@ int main (void)
     char     lmsg[80];
     snprintf(lmsg, sizeof(lmsg),
        "\r\n\n "        // example:ae747e8
-    "Ruuvi Main APP_FW:" APP_FW_VERSION " compiled_on:%s at %s; MAC:%4X%08lX  \r\n",
-                                                    __DATE__, __TIME__, // example:Feb 19 2021  15:46:46
-                            (uint16_t)( 0xC000 | (NRF_FICR->DEVICEADDR[1] & 0x0000FFFF)),
-                                                                NRF_FICR->DEVICEADDR[0]);
+    "Ruuvi Main APP_FW:" APP_FW_VERSION " compiled_on:%s at %s  \r\n",
+                                                    __DATE__, __TIME__); // example:Feb 19 2021  15:46:46
     LOG(lmsg);
   
 #   if RUUVI_RUN_TESTS
