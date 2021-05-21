e54c4827e02b862984c1e120c29b70e0ac2e4562
Otso Jousimaa <otso@ojousima.net>
2021-05-21 17:55:00 +0300

    Update date for evefy bcast

--- src/application_config/application_mode_default.h
+++ src/application_config/application_mode_default.h
@@ -32,7 +32,7 @@
 
 /** @brief repeat same data N times in advertisement, reduce sensor reads. */
 #ifndef APP_NUM_REPEATS
-#   define APP_NUM_REPEATS 2
+#   define APP_NUM_REPEATS 1
 #endif
 
 /** @brief Refresh sensor data at this interval.*/
