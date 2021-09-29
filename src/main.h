fda02fbf5647d07c90e79d1e1b031d1b522ff2bd
Otso Jousimaa <otso@ojousima.net>
2021-09-29 13:26:01 +0300

    Update endpoints

--- src/main.h
+++ src/main.h
@@ -24,7 +24,7 @@
 // Submodule requirements
 #define RUUVI_BOARDS_REQ "3.7.1"
 #define RUUVI_DRIVERS_REQ "3.9.0"
-#define RUUVI_ENDPOINTS_REQ "3.0.0"
+#define RUUVI_ENDPOINTS_REQ "3.1.0"
 #define RUUVI_LIBRARIES_REQ "3.0.0"
 
 #define APP_SELFTEST_OK_DELAY_MS (1000U) //!< time to show "ok" led.
