b4f4991729d1e1bcb8b6128cb3a0b8f14a8f9101
Otso Jousimaa <otso@ojousima.net>
2021-10-05 17:20:15 +0300

    DF3 encode pass

--- src/app_dataformats.c
+++ src/app_dataformats.c
@@ -1,4 +1,13 @@
 #include "app_dataformats.h"
+#include "app_sensor.h"
+#include "ruuvi_endpoints.h"
+#include "ruuvi_endpoint_3.h"
+#include "ruuvi_endpoint_5.h"
+#include "ruuvi_endpoint_8.h"
+#include "ruuvi_endpoint_fa.h"
+#include "ruuvi_task_adc.h"
+
+#include <math.h>
 
 #ifdef CEEDLING
 #   define TESTABLE_STATIC
@@ -24,7 +33,18 @@ encode_to_3 (uint8_t * const output,
              size_t * const output_length,
              const rd_sensor_data_t * const data)
 {
-    return RD_ERROR_NOT_IMPLEMENTED;
+    rd_status_t err_code = RD_SUCCESS;
+    re_3_data_t ep_data = {0};
+    ep_data.accelerationx_g   = rd_sensor_data_parse (data, RD_SENSOR_ACC_X_FIELD);
+    ep_data.accelerationy_g   = rd_sensor_data_parse (data, RD_SENSOR_ACC_Y_FIELD);
+    ep_data.accelerationz_g   = rd_sensor_data_parse (data, RD_SENSOR_ACC_Z_FIELD);
+    ep_data.humidity_rh       = rd_sensor_data_parse (data, RD_SENSOR_HUMI_FIELD);
+    ep_data.pressure_pa       = rd_sensor_data_parse (data, RD_SENSOR_PRES_FIELD);
+    ep_data.temperature_c     = rd_sensor_data_parse (data, RD_SENSOR_TEMP_FIELD);
+    err_code |= rt_adc_vdd_get (&ep_data.battery_v);
+    re_3_encode (output, &ep_data, RD_FLOAT_INVALID);
+    *output_length = RE_3_DATA_LENGTH;
+    return err_code;
 }
 
 TESTABLE_STATIC rd_status_t
@@ -67,5 +87,22 @@ rd_status_t app_dataformat_encode (uint8_t * const output,
                                    size_t * const output_length,
                                    const app_dataformat_t format)
 {
-    return RD_ERROR_NOT_IMPLEMENTED;
+    rd_status_t err_code = RD_SUCCESS;
+    rd_sensor_data_t data = {0};
+    data.fields = app_sensor_available_data();
+    float data_values[rd_sensor_data_fieldcount (&data)];
+    data.data = data_values;
+    app_sensor_get (&data);
+
+    switch (format)
+    {
+        case DF_3:
+            encode_to_3 (output, output_length, &data);
+            break;
+
+        default:
+            err_code |= RD_ERROR_NOT_IMPLEMENTED;
+    }
+
+    return err_code;
 }
