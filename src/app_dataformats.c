31085ff800e2af2e00623bfe2624679d20f96b07
Otso Jousimaa <otso@ojousima.net>
2021-10-08 16:44:07 +0300

    Remove duplicate docs

--- src/app_dataformats.c
+++ src/app_dataformats.c
@@ -61,13 +61,6 @@ uint32_t app_data_encrypt (const uint8_t * const cleartext,
     return ret_code;
 }
 
-/**
- * @brief Return next dataformat to send
- *
- * @param[in] formats Enabled formats.
- * @param[in] state Current state of dataformat picker.
- * @return    Next dataformat to use in app.
- */
 app_dataformat_t app_dataformat_next (const app_dataformats_t formats,
                                       const app_dataformat_t state)
 {
@@ -223,18 +216,6 @@ encode_to_fa (uint8_t * const output,
     return err_code;
 }
 
-/**
- * @brief Encode data into given buffer with given format.
- *
- * A call to this function will increment measurement sequence counter
- * where applicable. Sensors are read to get latest data from board.
- *
- * @param[out] output Buffer to which data is encoded.
- * @param[in,out] output_length Input: Size of output buffer.
- *                              Output: Size of encoded data.
- * @param[in] format Format to encode data into.
- *
- */
 rd_status_t app_dataformat_encode (uint8_t * const output,
                                    size_t * const output_length,
                                    const app_dataformat_t format)
