0c6b8104ef3d8a8da1cb716d7bc79ae8cf745163
Dennis German <DGermang@Real-World-Systems.com>
2021-10-19 12:49:03 -0400

    Add comments and usage information to application_modes.h (#296)
    Thank you for the contribution

--- src/application_config/application_modes.h
+++ src/application_config/application_modes.h
@@ -1,13 +1,18 @@
 /**
- * Select application mode definitions to load.
- * Ideally the desired mode is passed as a makefile flag.
+ * @file application_config/application_modes.h
+ * @brief application mode definitions
+ * mode MAYBE passed as a makefile flag ONE of: -DDEBUG | -DAPPLICATION_MODE_LONGLIFE | -DAPPLICAITON_MODE_LONGMEM
  *
- * License: BSD-3
- * Author: Otso Jousimaa <otso@ojousima.net>
+ * @copyright Ruuvi Innovations Ltd, license BSD-3-Clause.
+ * @author: Otso Jousimaa <otso@ojousima.net>
+ *          DG12 <DGerman@Real-World-Systems.com>
+ * @date 2021-07-24 add more comments and example
+ *
+ * Customizations do NOT go here
  */
-
 #ifndef APPLICATION_MODES_H
 #define APPLICATION_MODES_H
+
 #ifdef APPLICATION_MODE_LONGLIFE
 #include "application_mode_longlife.h"
 #elif APPLICATION_MODE_LONGMEM
@@ -15,5 +20,29 @@
 #elif DEBUG
 #include "application_mode_debug.h"
 #endif
-#include "application_mode_default.h"
-#endif
+
+#include "application_mode_default.h"   // always add in the Default settings (perhaps superceeded by above)
+
+#endif  //  APPLICATION_MODES_H
+
+
+
+//  // // // // // // // // // // // // // // // // // // // //
+// Example: debug:   (partial)
+//
+//  #define APP_FW_VARIANT "+debug"
+//
+//  #define RI_LOG_ENABLED 1
+//  #define SEGGER_RTT_CONFIG_DEFAULT_MODE 2 // block 2 // trim 1 // skip 0 // if LOG buffer is full
+//
+//  #define APP_NUM_REPEATS                  4  // advertise the same data 4 times instead of default 1.
+//  #define APP_LOG_INTERVAL_S             (1u) // save history every second rather than the default 5
+//  #define APP_BLE_INTERVAL_MS          (221u) // advertise faster then default
+//
+// Example from default:  (partial)
+//
+//  #ifndef APP_BLE_INTERVAL_MS              // (Which is superceeded at  221 above)
+//  #define APP_BLE_INTERVAL_MS (1285u)          /** advertise sensor data at this interval. 1285 matches Apple guideline. */
+//  #endif
+//
+//END
