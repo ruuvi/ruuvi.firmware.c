4572d1d547ef40e84351d01037929580e98dc7d1
ojousima <otso@ojousima.net>
2021-11-04 08:57:57 +0000

    Deploying to gh-pages from @ ruuvi/ruuvi.firmware.c@a755315348741d7eabebef7823386771d49794a3 🚀

--- app__dataformats_8h.js
+++ app__dataformats_8h.js
@@ -8,6 +8,6 @@ var app__dataformats_8h =
       [ "DF_8", "app__dataformats_8h.html#af2622d100600a892b45980ca1b32e05aa7a63b033df71ced27b3c0dee5cc5fae9", null ],
       [ "DF_FA", "app__dataformats_8h.html#af2622d100600a892b45980ca1b32e05aa211c029a0d5bd5545ef19fcc0a624a19", null ]
     ] ],
-    [ "app_dataformat_encode", "app__dataformats_8h.html#abf284c4ef1f3236a146bc531f62b439d", null ],
+    [ "app_dataformat_encode", "app__dataformats_8h.html#a1e7eea975317314d1ba399fa1759cd0c", null ],
     [ "app_dataformat_next", "app__dataformats_8h.html#aba3a37e3de86440a0dac8769388aa8fb", null ]
 ];
