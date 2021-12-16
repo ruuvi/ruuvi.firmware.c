638b5061233c9b631153aa32288dac57abfe7b51
Dennis German <DGermang@Real-World-Systems.com>
2021-12-16 00:11:20 -0500

    trailing space
    Argh!

--- test/test_app_log.c
+++ test/test_app_log.c
@@ -1,6 +1,6 @@
 /* @file test/test_app_log.c
  * @date 
- *       2021-12-15 DG remove test for unused functions: 
+ *       2021-12-15 DG remove test for unused functions:
  *                      app_log_config_get/set
  */
 #include "unity.h"
