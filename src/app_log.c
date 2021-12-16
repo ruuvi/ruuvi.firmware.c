0e0ffa24daa120f6e5def940d2e484ea9c7a664c
Dennis German <DGermang@Real-World-Systems.com>
2021-12-16 00:26:34 -0500

    remove un used functions

--- src/app_log.c
+++ src/app_log.c
@@ -6,7 +6,7 @@
  *       2021-05-06 includes Potential fix for #255 (#256)
  * @brief
  * Save and retrieve sensor readings to/from flash
- *  for transmitting to station for sync.
+ *   for transmitting to station for sync.
  * @detail
  * Allocate static input, output and config memory.
  * Readings are blocked into a nearly page size buffer.
