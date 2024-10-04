a8f9cd8f4290941d4f7835f23fed3517dc94f24c
Dennis German <DGermang@Real-World-Systems.com>
2024-10-04 10:21:33 -0400

    Config file touch-up (#327)
    Sonar does not run because PR origin is external, ok to merge.

--- src/application_config/application_mode_debug.h
+++ src/application_config/application_mode_debug.h
@@ -2,7 +2,7 @@
 #define APPLICATION_MODE_DEBUG_H
 
 #define APP_FW_VARIANT "+debug"
-#define RI_LOG_ENABLED 1
+#define RI_LOG_ENABLED (true)
 #define APP_LOG_LEVEL RI_LOG_LEVEL_INFO
 #define APP_WDT_INTERVAL_MS (10U*60U*1000U)
 #define ENABLE_ALL_DATAFORMATS (1U)
@@ -11,11 +11,11 @@
 #define APP_BLE_INTERVAL_MS (221U)
 
 /** @brief repeat same data N times in advertisement, reduce sensor reads. */
-#define APP_NUM_REPEATS 4
+#define APP_NUM_REPEATS (4U)
 
 /** @brief Communicate sensor data at this interval. 221 matches Apple guideline. */
 #define APP_HEARTBEAT_INTERVAL_MS (APP_BLE_INTERVAL_MS * APP_NUM_REPEATS)
 
 #define APP_LOG_INTERVAL_S (1U) //!< Gets limited to heartbeat rate.
 
-#endif
+#endif // APPLICATION_MODE_DEBUG_H
